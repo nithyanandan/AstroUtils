326cdfa524ce1de0199cdc27f498b358ff55a278
nithyanandan <nithyanandan.t@gmail.com>
2016-03-25 16:04:42 -0700

    Start making an installable package
    From c1a87db (pkg):
    ==================
    
    (*) Start making it an installable package

---
+++ generalutils/__init__.py
@@ -0,0 +1,4 @@
+from generalutils import catalog, constants, ephemeris_timing
+from generalutils import foregrounds, geometry, lookup_operations
+from generalutils import my_DSP_modules, my_gridding_modules
+from generalutils import my_operations, writer_module
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='generalutils',
+      version='0.1.0',
+      description='General Purpose Utilies',
+      long_description=open("README.txt").read(),
+      url='http://github.com/nithyanandan/general',
+      author='Nithyanandan Thyagarajan',
+      author_email='nithyanandan.t@gmail.com',
+      license='MIT',
+      packages=['generalutils'],
+      include_package_data=True,
+      zip_safe=False)
