d0603dc4eb0ee3c7ca9021348ae378644bf873e3
nithyanandan <nithyanandan.t@gmail.com>
2016-04-06 02:08:51 -0700

    Modify indentation to 4 spaces in setup.py
    From ec2b1d6 (pkg):
    ==================
    
    In setup.py,
    
    (*) Modify indentation to 4 spaces

--- setup.py
+++ setup.py
@@ -6,17 +6,17 @@ metafile = open('./generalutils/__init__.py').read()
 metadata = dict(re.findall("__([a-z]+)__\s*=\s*'([^']+)'", metafile))
 
 setup(name='generalutils',
-      version=metadata['version'],
-      description=metadata['description'],
-      long_description=open("README.txt").read(),
-      url=metadata['url'],
-      author=metadata['author'],
-      author_email=metadata['authoremail'],
-      license='MIT',
-      packages=find_packages(),
-      include_package_data=True,
-      install_requires=['blessings'],
-      setup_requires=['blessings', 'pytest-runner'],
-      tests_require=['pytest'],      
-      zip_safe=False)
+    version=metadata['version'],
+    description=metadata['description'],
+    long_description=open("README.txt").read(),
+    url=metadata['url'],
+    author=metadata['author'],
+    author_email=metadata['authoremail'],
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['blessings'],
+    setup_requires=['blessings', 'pytest-runner'],
+    tests_require=['pytest'],      
+    zip_safe=False)
 
