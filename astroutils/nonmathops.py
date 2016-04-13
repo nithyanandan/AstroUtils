3ea71e12f5fb2c281002d2dc9f4fc7f3ea167b6f
nithyanandan <nithyanandan.t@gmail.com>
2016-04-13 01:02:28 -0700

    Add is_dict1_subset_of_dict2() - nonmathops module
    From 5190e34 (nonmath):
    ======================
    
    In astroutils/nonmathops.py,
    
    (*) Add function is_dict1_subset_of_dict2() to check if one
    dictionary is a subset of another. Also create docstring
    
    In astroutils/test/nonmathops_test.py,
    
    (*) Add tests test_is_dict1_subset_of_dict1_case1() and
    test_is_dict1_subset_of_dict1_case2() to test the
    function is_dict1_subset_of_dict1() in the module nonmathops.py

--- astroutils/nonmathops.py
+++ astroutils/nonmathops.py
@@ -15,6 +15,9 @@ def recursive_find_notNone_in_dict(inpdict):
     ----------------------------------------------------------------------------
     """
     
+    if not isinstance(inpdict, dict):
+        raise TypeError('inpdict must be a dictionary')
+
     outdict = {}
     for k, v in inpdict.iteritems():
         if v is not None:
@@ -27,3 +30,57 @@ def recursive_find_notNone_in_dict(inpdict):
     return outdict
 
 ################################################################################
+
+def is_dict1_subset_of_dict2(dict1, dict2, ignoreNone=True):
+
+    """
+    ----------------------------------------------------------------------------
+    Check if keys and values of the first dictionary are a subset of the second.
+
+    Inputs:
+
+    dict1       [dictionary] First dictionary. It will be checked if both its
+                keys and values are a subset of the second dictionary.
+
+    dict2       [dictionary] Second dictionary. The values and keys of the first
+                dictionary will be checked against this dictionary to check if
+                the first is a subset of the second. 
+
+    ignoreNone  [boolean] If set to True (default), the subset checking happens
+                using the non-None values in both dictionaries. This is a 
+                loose check. If set to False, a strict subset checking happens
+                not ignoring the None values, if any.
+
+    Output: 
+
+    Boolean value True if dict1 is found to be a subset of dict2, False 
+    otherwise
+    ----------------------------------------------------------------------------
+    """
+    
+    if not isinstance(dict1, dict):
+        raise TypeError('Input dict1 must be a dictionary')
+
+    if not isinstance(dict2, dict):
+        raise TypeError('Input dict2 must be a dictionary')
+
+    if ignoreNone:
+        dict1 = recursive_find_notNone_in_dict(dict1)
+        dict2 = recursive_find_notNone_in_dict(dict2)
+
+    if cmp(dict1, dict2) == 0:
+        return True
+    else:
+        dict2sub = {}
+        for k, v in dict1.iteritems():
+            if k in dict2:
+                dict2sub[k] = dict2[k]
+            else:
+                return False
+        if cmp(dict1, dict2sub) == 0:
+            return True
+        else:
+            return False
+
+################################################################################
+
--- astroutils/test/nonmathops_test.py
+++ astroutils/test/nonmathops_test.py
@@ -11,3 +11,15 @@ def test_recursive_find_notNone_in_allNone_dict():
     expdict = {}
     outdict = NMO.recursive_find_notNone_in_dict(inpdict)
     assert cmp(expdict, outdict) == 0
+
+def test_is_dict1_subset_of_dict1_case1():
+    dict1 = {'a': None, 'b': [0,1], 'c': {'c1': 1, 'c2': {'c21': 21, 'c22': 22}}}
+    dict2 = {'b': [0,1], 'c': {'c1': 1, 'c2': {'c21': 21, 'c22': 22}}, 'd': 'xyz'}
+    assert NMO.is_dict1_subset_of_dict2(dict1, dict2, ignoreNone=True)
+    assert not NMO.is_dict1_subset_of_dict2(dict1, dict2, ignoreNone=False)
+
+def test_is_dict1_subset_of_dict1_case2():
+    dict1 = {'a': None, 'b': [0,1], 'c': {'c1': 1, 'c2': {'c21': 21, 'c22': 22}}}
+    dict2 = {'a': 0, 'b': [0,1], 'c': {'c1': 1, 'c2': {'c21': 21, 'c22': 22}}, 'd': 'xyz'}
+    assert NMO.is_dict1_subset_of_dict2(dict1, dict2, ignoreNone=True)
+    assert not NMO.is_dict1_subset_of_dict2(dict1, dict2, ignoreNone=False)
