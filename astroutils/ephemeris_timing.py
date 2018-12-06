29c310ac541c7b9aa7fe1e287667897e17286fbf
Nithyanandan Thyagarajan <nthyagar@griffin.aoc.nrao.edu>
2018-12-05 17:13:26 -0700

    Add julian_date_from_LAST() in ephemeris_timing.py
    From e1af3b1 (ephermeris):
    =========================
    
    In astroutils/ephemeris_timing.py,
    
    (*) Add function julian_date_from_LAST() with docstring
    (*) Update docstring of function equation_of_equinoxes()

--- astroutils/ephemeris_timing.py
+++ astroutils/ephemeris_timing.py
@@ -9,28 +9,99 @@ import ephem as EP
 def equation_of_equinoxes(jd):
     """
     ----------------------------------------------------------------------------
+    Estimate the equation of the equinoxes
     Inputs:
 
-    jd      [numpy array] Julian date at which nutation is to be estimated and
-            the equation of equinoxes returned. 
+    jd      [scalar or numpy array] Julian date at which nutation is to be 
+            estimated and the equation of equinoxes is returned. 
 
     Output:
 
     Equation of the equinoxes (in hours) that should be used to correct the 
     Greenwich Mean Sidereal Time to obtain the Greenwich Apparent Sidereal Time
+
+    Notes: Adopted from https://aa.usno.navy.mil/faq/docs/GAST.php
     ----------------------------------------------------------------------------
     """
+
+    if not isinstance(jd, (int, float, NP.ndarray)):
+        raise TypeError('Input julian date(s) must be a scalar or numpy array')
 
     d = jd - 2451545.0 # Days since 2000 January 1, 12h UT, Julian date 2451545.0
     omega = 125.04 - 0.052954 * d # Longitude of the ascending node of the Moon in degrees
     l = 280.47 + 0.98565 * d # Mean Longitude of the Sun in degrees
-    obliquity = 23.4393-- 0.0000004 * d # in degrees
+    obliquity = 23.4393 - 0.0000004 * d # in degrees
 
     nutation = -0.000319 * NP.sin(NP.radians(omega)) - 0.000024 * NP.sin(NP.radians(2*l))  # in hours
     eqeq = nutation * NP.cos(NP.radians(obliquity)) # Equation of the equinoxes in hours
     # t = d / 36525 # number of centuries since 2000 January 1, 12h UT, Julian date 2451545.0
 
     return eqeq
+
+################################################################################
+
+def julian_date_from_LAST(last, jd0, longitude, tol=1e-6):
+    """
+    ----------------------------------------------------------------------------
+    Inputs:
+
+    last        [scalar or numpy array] Local Apparent Sidereal Time (in hours)
+
+    jd0         [scalar or numpy array] Julian date at previous midnight. Same
+                size as input 'last' or numpy broadcastable to that shape
+
+    longitude   [scalar or numpy array] Longitude of observing site (in hours). 
+                Same size as input 'last' or numpy broadcastable to that shape
+
+    tol         [scalar] Tolerance for convergence since these calculations
+                are iteratively solved
+
+    Output:
+
+    Julian date(s) as a numpy array correspoding to the input apparent sidereal
+    time and longitude on given starting Julian dates.
+
+    Notes: Adopted from https://aa.usno.navy.mil/faq/docs/GAST.php
+    ----------------------------------------------------------------------------
+    """
+
+    if not isinstance(jd0, (int, float, NP.ndarray)):
+        raise TypeError('Input starting julian date(s) must be a scalar or numpy array')
+    jd0 = NP.asarray(jd0).ravel()
+
+    if not isinstance(last, (int, float, NP.ndarray)):
+        raise TypeError('Input local apparent sidereal time(s) must be a scalar or numpy array')
+    last = NP.asarray(last).ravel()
+
+    if not isinstance(longitude, (int, float, NP.ndarray)):
+        raise TypeError('Input longitude(s) must be a scalar or numpy array')
+    longitude = NP.asarray(longitude).ravel()
+
+    jd = NP.copy(jd0)
+    gast = last + longitude
+
+    if gast < 0.0:
+        gast += 24.0
+    if gast >= 24.0:
+        gast -= 24.0
+
+    dev = 100 * tol
+    while (iter < 1000) and (NP.abs(dev) > tol):
+        d = jd - 2451545.0
+        t = d / 36525.0
+        eqeq = equation_of_equinoxes(jd)
+        gmst = gast - eqeq
+        if gmst < 0.0:
+            gmst += 24.0
+        if gmst >= 24.0:
+            gmst -= 24.0
+        
+        newhr = (gmst - 6.697374558 - 0.06570982441908 - 0.000026 * t**2) / 1.00273790935
+        dev = (newhr - hr) / hr
+        hr = NP.copy(newhr)
+        jd = jd0 + hr/24.0
+
+    return jd
 
 ################################################################################
 
